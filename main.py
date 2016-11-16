d62b45ad9cda09b2f1fe877c547f67bd3848cc51
HollyMadCow <zlwh2y@163.com>
2016-11-16 22:25:04 +0800

    test

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
-import random, string
+import random
+import string
 from flask import Flask
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
